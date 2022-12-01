27de212647119fcf719d181a4c163f2cdd9d5f91
Zoolian <gamebwoy13@gmail.com>
2022-12-01 23:25:23 +0000

    add 2arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     size = len(sys.argv)
-    if size == 1:
-        print("0 arguments.")
+    if (size == 1):
+        print((size - 1), "arguments.")
     elif size == 2:
-        print("1 argument:")
+        print((size - 1), "argument:")
         print("1: {}".format(sys.argv[1]))
-    elif size > 2:
-        print("{} arguments".format(size - 1))
+    else:
+        print("{} arguments:".format(size - 1))
         for i in range(1, size):
             print("{}: {}".format(i, sys.argv[i]))
