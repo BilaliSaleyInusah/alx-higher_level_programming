fb5ef61da4670a5707e2f2ebe9e20cb5ec1c7bd2
Zoolian <gamebwoy13@gmail.com>
2022-12-01 22:14:12 +0000

    adding 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+size = len(sys.argv)
+if size == 1:
+    print("0 arguments.")
+elif size == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+elif size > 2:
+    print("{} arguments".format(size - 1))
+    for i in range(1, size):
+        print("{}: {}".format(i, sys.argv[i]))
