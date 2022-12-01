df263bfa3f558151c713e00c34c79d3bc8bce66d
Zoolian <gamebwoy13@gmail.com>
2022-12-01 21:07:11 +0000

    adding1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def main():
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
@@ -7,5 +7,3 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-if __name__ == "__main__":
-    main()
