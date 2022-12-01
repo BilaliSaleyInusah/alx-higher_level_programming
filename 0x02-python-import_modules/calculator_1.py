1e1c9cb249245cde7a961d93e670782dc59fc994
Zoolian <gamebwoy13@gmail.com>
2022-12-01 20:49:13 +0000

    adda

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Args:
+    a: first inteer
+    b: second integer
+
+add - return a + b
+sub - return a - b
+mul - return a * b
+div - return a / b
+"""
+def add(a, b):
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return (a / b)
+
