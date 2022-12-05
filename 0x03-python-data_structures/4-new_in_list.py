f0b171e72f52baf08df26e32416282fab92f1c36
Zoolian <gamebwoy13@gmail.com>
2022-12-05 20:53:38 +0000

    adding 4b

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 def new_in_list(my_list, idx, element):
     new_list = []
-    for i in my_list:
-        new_list.append(i)
+    for i in range(len(my_list)):
+        new_list[i] = my_list[i]
     if idx >= 0 and idx < len(my_list):
         new_list[idx] = element
     return new_list
